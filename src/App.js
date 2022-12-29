7042cb3a60ca086371dfe9a94232524be9974a72
naresh63 <nvalvi63@gmail.com>
2022-12-29 20:29:45 +0530

    etherjs project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import { ethers } from "ethers";
+
+
 
 function App() {
+  const [enterAddr, setEnterAddr] = useState("");
+  const [block, setBlock] = useState("");
+  const [accBal, setAccBal] = useState("");
+  const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/448893adf466452f8119284681c3a026`);
+
+  const querryBlockchain = async ()=>{
+    const currBlock = await provider.getBlockNumber();
+    setBlock(currBlock);
+     const balance = await provider.getBalance(enterAddr);
+     const balInEther = ethers.utils.formatEther(balance)
+     setAccBal(balInEther);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Check ether balance of any ethereum address</h1>
+     <input className='inp-addr' type="text" value={enterAddr} onChange={(e)=>setEnterAddr(e.target.value)} placeholder="enter address/ENS"/>
+      <button className='search-addr-btn' onClick={()=>querryBlockchain()}> search</button>
+      <p> e.g. 0x8ba1f109551bD432803012645Ac136ddd64DBA72</p>
+      <h3> latest block : <span className='block-value'> {block}</span> </h3>
+      {
+        accBal  &&   <h3> balance for given address <span className='addr-value'>  {enterAddr}</span> is : <span className='bal-value'> {accBal} </span> Ether</h3>   
+      }
     </div>
   );
 }
